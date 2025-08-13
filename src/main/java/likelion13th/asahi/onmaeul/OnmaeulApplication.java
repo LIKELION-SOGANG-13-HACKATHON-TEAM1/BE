8974b89471e714a3d8d96f290fbe7acb1cab9e48
kayunni <gykim0129@gmail.com>
2025-08-13 19:55:31 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/likelion13th/asahi/onmaeul/OnmaeulApplication.java
@@ -0,0 +1,13 @@
+package likelion13th.asahi.onmaeul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnmaeulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnmaeulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion13th/asahi/onmaeul/OnmaeulApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion13th.asahi.onmaeul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnmaeulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
